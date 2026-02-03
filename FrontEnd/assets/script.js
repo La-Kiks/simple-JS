8f55f738387d0f809e22ccf20cc355b2b61f1b50
La-Kiks <kilian.audroin@gmail.com>
2026-02-03 17:03:31 +0100

    Chargement des WORKS et gestion des filtres

---
+++ FrontEnd/assets/script.js
@@ -0,0 +1,53 @@
+const API_BASE_URL = "http://localhost:5678/api";
+
+const gallery = document.getElementById("gallery");
+const filterButtons = document.querySelectorAll("#filters button");
+
+let works = [];
+
+function displayWorks(worksToDisplay) {
+  gallery.innerHTML = "";
+
+  worksToDisplay.forEach((work) => {
+    gallery.innerHTML += `
+        <figure>
+					<img src="${work.imageUrl}" alt="${work.title}">
+					<figcaption>${work.title}</figcaption>
+				</figure>
+        `;
+  });
+}
+
+async function fetchWorks() {
+  try {
+    const response = await fetch(API_BASE_URL + "/works");
+    works = await response.json();
+
+    displayWorks(works);
+  } catch (e) {
+    console.error("Error : ", e);
+  }
+}
+
+function filterWorksByCategory(categoryId) {
+  if (categoryId === "all") {
+    console.log("Filter All");
+    displayWorks(works);
+    return;
+  }
+
+  const filteredWorks = works.filter((work) => {
+    return work.categoryId === Number(categoryId);
+  });
+
+  displayWorks(filteredWorks);
+}
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    filterWorksByCategory(button.dataset.category);
+  });
+});
+
+// Execution :
+fetchWorks();
